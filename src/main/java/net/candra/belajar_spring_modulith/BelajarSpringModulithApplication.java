6d8ae8d3e4ebd408a6675eab1c58c2a42129510c
Candra Hermanto <candrahermanto@Candras-MacBook-Air.local>
2024-11-24 16:20:26 +0700

    initial spring modulith application

---
+++ src/main/java/net/candra/belajar_spring_modulith/BelajarSpringModulithApplication.java
@@ -0,0 +1,13 @@
+package net.candra.belajar_spring_modulith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringModulithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringModulithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/candra/belajar_spring_modulith/BelajarSpringModulithApplicationTests.java
@@ -0,0 +1,13 @@
+package net.candra.belajar_spring_modulith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringModulithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
